1921d1c6b07e775d14fb363a9665d29c03d5e719
Li Xiong <xiongli.cs@gmail.com>
2014-12-15 17:14:07 -0800

    tree

--- Tree/tree.cpp
+++ Tree/tree.cpp
@@ -8,6 +8,7 @@
 #include<set>
 #include<stdlib.h>
 #include<list>
+#include<stack>
 using namespace std;
 
 struct TreeNode
@@ -78,6 +79,70 @@ void preorder(TreeNode *rt)
     }
 }
 
+void inorder(TreeNode* rt)
+{
+    if(rt->left)
+        inorder(rt->left);
+    printf("%d ", rt->val);
+    if(rt->right)
+        inorder(rt->right);
+}
+
+void postorder(TreeNode *rt)
+{
+    if(rt->left)
+        postorder(rt->left);
+    if(rt->right)
+        postorder(rt->right);
+    printf("%d ", rt->val);
+}
+
+void inorder_iterative(TreeNode *rt)
+{
+   if(!rt) return; //empty tree, edge case
+   stack<TreeNode*>stk;
+   stk.push(rt);
+   while(rt->left)
+   {
+       stk.push(rt->left);
+       rt = rt->left;
+   }
+
+   while(!stk.empty())
+   {
+       TreeNode* cur = stk.top();
+       printf("%d ", cur->val);
+       stk.pop();
+       if(cur->right)
+       {
+           stk.push(cur->right);
+           TreeNode* tmp = cur->right;
+           while(tmp->left)
+           {
+               stk.push(tmp->left);
+               tmp = tmp->left;
+           }
+       }
+   }
+   printf("\n");
+}
+
+void preorder_iterative(TreeNode *rt)
+{
+    if(!rt) return;
+    stack<TreeNode*>stk;
+    stk.push(rt);
+    while(!stk.empty())
+    {
+        TreeNode* cur = stk.top();
+        printf("%d ", cur->val);
+        stk.pop();
+        if(cur->right) stk.push(cur->right);
+        if(cur->left) stk.push(cur->left);
+    }
+    printf("\n");
+}
+
 TreeNode* inorderSuccessor(TreeNode *rt, TreeNode *target)
 {
     if(target->right)
@@ -87,19 +152,17 @@ TreeNode* inorderSuccessor(TreeNode *rt, TreeNode *target)
             tmp = tmp ->left;
         return tmp;
     }
-    
-    TreeNode *tmpRt = rt;
-    TreeNode *succ = NULL;
-    while(tmpRt)
+    TreeNode* succ = NULL;
+    while(rt)
     {
-        if(target->val < tmpRt->val)
+        if(rt->val < target->val)
         {
-            succ = tmpRt;
-            tmpRt = tmpRt -> left;
+            rt = rt -> right;
         }
-        else 
+        else
         {
-            tmpRt = tmpRt -> right;
+            succ = rt;
+            rt = rt -> left;
         }
     }
     return succ;
@@ -118,10 +181,22 @@ TreeNode* findTarget(TreeNode *rt, int value)
     }
     return NULL;
 }
-void inorder_iterative(TreeNode *rt)
+
+bool isValidBST(TreeNode* rt)
 {
-    //to do
+    static TreeNode* pre = NULL;
+    if(rt)
+    {
+        if(!isValidBST(rt->left))
+            return false;
+        if(pre != NULL && rt->val <= pre->val)
+            return false;
+        pre = rt;
+        return isValidBST(rt->right);
+    }
+    return true;
 }
+
 int main()
 {
     int a[] = {1,3,4,6,7,8,10,13,14}; //from wikipedia
@@ -136,10 +211,41 @@ int main()
     TreeNode* rt1 = sortedArrayToBST(a,0,n-1);
     TreeNode* rt2 = sortedLinkedListToBST(it,0,n-1);
     
+    TreeNode* test = new TreeNode(0);
+    TreeNode* test_left = new TreeNode(1);
+    test->left = test_left;
+    
+    preorder_iterative(test);
+    bool isValid = isValidBST(test);
+    if(isValid)
+        printf("valid bst\n");
+    else 
+        printf("invalid bst\n");
+    printf("inorder recursive traverse a tree: ");
+    inorder(rt0);
+    printf("\n");
+
+    printf("inorder iterative traverse a tree: ");
+    inorder_iterative(rt0);
+    printf("\n");
+
+    printf("preorder recursive traverse a tree: ");
+    preorder(rt0);
+    printf("\n");
+
+    printf("preorder iterative traverse a tree: ");
+    preorder_iterative(rt0);
+    printf("\n");
+
+    printf("postorder recursive traverse a tree: ");
+    postorder(rt0);
+    printf("\n");
+
     TreeNode* target = findTarget(rt0, 6);
 
     TreeNode* succ = inorderSuccessor(rt0, target);
     printf("succ of %d is %d\n",target->val,  succ->val);
+
     preorder(rt0);
     printf("\n");    
     preorder(rt1);
